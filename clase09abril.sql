8161645612cd5283ff98984a42180f64ec193258
sergiochile <sergio.nlevicoy@gmail.com>
2025-04-09 17:07:43 +0200

    EJERCICIOS CLASE 9

--- clase09abril.sql
+++ clase09abril.sql
@@ -3,4 +3,5 @@ select UPPER(APELLIDO) as APELLIDO from EMP;
 
 --Construir una consulta para que salga la fecha de hoy con el siguiente formato: Martes 06 de Octubre de 2020 
 select to_char(sysdate, 'Day DD "de" Month "de" YYYY') as FECHA from DUAL; -- Esto nos mostrara la fecha de hoy con el siguiente formato: Martes 06 de Octubre de 2020
-select sysdate as FECHA_ACTUAL from DUAL; --ESTO MUESTRA LA FECHA ACTUAL DEL SISTEMA
+select sysdate as FECHA_ACTUAL from DUAL; --ESTO MUESTRA LA FECHA ACTUAL DEL SISTEMA.
+
